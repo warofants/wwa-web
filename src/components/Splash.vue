eb0428a4a83aa58dc63e183c7ec8d4558b3b661d
Nils Blömeke <bloemeni@outlook.com>
2019-08-14 03:52:01 +0200

    Initial Vue base
    Contains Bootstrap-vue for styling and run and watch script. Also includes router

---
+++ src/components/Splash.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="splash">
+    <div class="wrapper">
+      <div class="info-column">
+        <h1>World War of Ants</h1>
+        <div class="description">
+          World War of Ants is a open-source game
+        </div><br>
+        <div class="actions">
+            <b-button href="https://github.com/warofants/wwa" target="_blank">Github</b-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      error() {
+        return this.$route.query.error;
+      },
+    },
+  }
+</script>
