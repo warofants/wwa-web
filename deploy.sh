1faf401235ebe9658f3e2b4580df29181141bfc7
Johnathan Irvin <IrvinJohnathan@gmail.com>
2019-09-01 17:24:40 -0400

    WIP: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:warofants/wwa-web.github.io.git gh-pages
+git push -f git@github.com:warofants/wwa-web.git gh-pages
 
 cd -
